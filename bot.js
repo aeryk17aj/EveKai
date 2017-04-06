b4260a1cbf44c4ba9b283968923b1ca418d6321c
Aeryk <aeryk17aj@gmail.com>
2017-04-06 01:31:19 -0400

    Split

--- bot.js
+++ bot.js
@@ -1,19 +1,10 @@
-//Ugh
-const express = require('express');
-const app = express();
-
 // Dependencies
 const Discordie = require('discordie');
 const readline = require('readline');
 
-// Plugins
-const consoleHandler = require('./plugins/consoleHandler');
-const messageHandler = require('./plugins/messageHandler');
-const logger = require('./plugins/logger');
-const osu = require('./plugins/osu');
-const cleverbot = require('./plugins/cleverbot');
-const music = require('./plugins/music');
-// const drawing = require('./plugins/drawing');
+// Handlers
+const consoleHandler = require('./handler/consoleHandler');
+const messageHandler = require('./handler/messageHandler');
 
 const config = require('./config');
 
@@ -26,9 +17,8 @@ const rl = readline.createInterface({
 	output: process.stdout
 });
 
-rl.on('line', c => {
-	consoleHandler.respond(c);
-});
+rl.on('line', c =>
+	consoleHandler.respond(c, client));
 
 client.connect({ token: process.env.BOT_TOKEN || require('./auth').loginToken });
 
@@ -38,45 +28,21 @@ client.Dispatcher.on(Events.GATEWAY_READY, e => { // eslint-disable-line no-unus
 	console.log('[System] Connected.');
 });
 
-client.Dispatcher.on(Events.DISCONNECTED, e => console.log(`[System] Connection interrupted. (${e.error})`));
-client.Dispatcher.on(Events.GATEWAY_RESUMED, () => console.log('[System] Connection resumed.'));
+client.Dispatcher.on(Events.DISCONNECTED, e =>
+	console.log(`[${getCurrentTime()}] [System] Connection interrupted. (${e.error})`));
 
-client.Dispatcher.on(Events.GUILD_MEMBER_ADD, e => {
-	e.guild.generalChannel.sendMessage(`Welcome, **${e.member.name}** to **${e.guild.name}**.`);
-});
+client.Dispatcher.on(Events.GATEWAY_RESUMED, () =>
+	console.log(`[${getCurrentTime()}] [System] Connection resumed.`));
 
-client.Dispatcher.on(Events.GUILD_MEMBER_REMOVE, e => {
-	e.guild.generalChannel.sendMessage(`**${e.user.username}** has left **${e.guild.name}**.`);
-});
+client.Dispatcher.on(Events.GUILD_MEMBER_ADD, e =>
+	e.guild.generalChannel.sendMessage(`Welcome, **${e.member.name}** to **${e.guild.name}**.`));
 
-/**
- * Fired every time a message is received by the bot.
- * @param {Object} e Event object
- */
-function onMessageCreate (e) {
-	if (!e) return;
-	const msg = e.message;
-	// TODO: Add idle mode, top priority listen
-	logger.init(msg);
-	messageHandler.respond(msg);
-	if (!msg || !msg.content) return;
-	osu.respond(msg);
-	cleverbot.init(msg);
-	music.respond(msg);
-	// drawing.respond(msg, client);
+client.Dispatcher.on(Events.GUILD_MEMBER_REMOVE, e =>
+	e.guild.generalChannel.sendMessage(`**${e.user.username}** has left **${e.guild.name}**.`));
+
+client.Dispatcher.on(Events.MESSAGE_CREATE, e =>
+	messageHandler.handle(e, client));
+
+function getCurrentTime () {
+	return new Date(Date.now()).toLocaleString('en-US');
 }
-
-client.Dispatcher.on(Events.MESSAGE_CREATE, onMessageCreate);
-
-exports.client = client;
-
-app.set('port', process.env.PORT || 5000);
-app.use(express.static('public'));
-
-app.get('/', (request, response) => {
-	response.send('Hello World!');
-});
-
-app.listen(app.get('port'), () => {
-	console.log("Bot with dummy web server is running at localhost:" + app.get('port'));
-});
---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+app.set('port', process.env.PORT || 5000);
+app.use(express.static('public'));
+
+app.get('/', (request, response) => {
+	response.send('Hello World!');
+});
+
+app.listen(app.get('port'), () => {
+	console.log("Bot with dummy web server is running at localhost:" + app.get('port'));
+});

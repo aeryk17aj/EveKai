9cbac064c9b1bbb625eeb3d7cdd299d5a2e4aee5
Aeryk <aeryk17aj@gmail.com>
2017-06-18 18:41:06 -0400

    Allow to talk as bot from command line

--- handler/consoleHandler.js
+++ handler/consoleHandler.js
@@ -3,6 +3,8 @@ const util = require('../util/botUtil');
 const logger = require('../plugins/logger');
 
 // const config = require('../config');
+let cg;
+let ctc;
 
 /**
  * Ahh the sweet land of no permission checks because the console is owner-only
@@ -21,6 +23,43 @@ function respond (msg, client) {
 		process.exit(0);
 	});
 
+	addCommandSentence('sg', a => {
+		const guilds = client.Guilds;
+		const foundGuild = guilds.find(g => g.name === a);
+		if (foundGuild) cg = foundGuild;
+		else util.log(`Guild '${a}' not found.`);
+		if (ctc) ctc = undefined;
+	});
+
+	addCommand('lsgtc', () => {
+		if (!cg) util.log('No guild was set.');
+		else util.log(cg.textChannels.map(tc => tc.name));
+	});
+
+	addCommandSentence('stc', a => {
+		if (!cg) return util.log('Set a Guild first.');
+		const tcs = cg.textChannels;
+		const foundChannel = tcs.find(c => c.name === a);
+		if (foundChannel) ctc = foundChannel;
+		else util.log(`Text channel '${a}' not found in ${cg.name}.`);
+	});
+
+	addCommand('slr', () => {
+		const latestMessage = client.Messages.toArray().reverse()[0];
+		cg = latestMessage.guild;
+		ctc = latestMessage.channel;
+		util.log(`Focus set to: ${cg.name} - #${ctc.name}`);
+	});
+
+	addCommand('resetFocus', () => {
+		cg = undefined; ctc = undefined;
+	});
+
+	addCommandSentence('say', a => {
+		if (!cg || !ctc) return util.log('No set Guild and/or text channel.');
+		else ctc.sendMessage(a);
+	});
+
 	addCommandSentence('eval', a => {
 		let result;
 		return new Promise(resolve => {
@@ -32,7 +71,7 @@ function respond (msg, client) {
 			if (v === 'Success') {
 				if (typeof result === 'string' || typeof result === 'number' || typeof result === 'boolean') util.log(result);
 				else if (Array.isArray(result)) util.log(result.join(', '));
-				else util.log('Eval Success');
+				// else util.log('Eval Success');
 			}
 		});
 	});

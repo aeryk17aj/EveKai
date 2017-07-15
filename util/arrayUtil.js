319e3df7be8fa7003f07ab02c1fd5a46b6b6705a
Aeryk <aeryk17aj@gmail.com>
2017-07-15 03:23:50 -0400

    Add more array utility functions

--- util/arrayUtil.js
+++ util/arrayUtil.js
@@ -7,4 +7,22 @@ function rInAr (ar) {
 	return ar[Math.floor(Math.random() * ar.length)];
 }
 
-module.exports = { rInAr };
+function shuffle (a) {
+	for (let i = a.length; i; i--) {
+		const j = Math.floor(Math.random() * i);
+		[a[i - 1], a[j]] = [a[j], a[i - 1]];
+	}
+	return a;
+}
+
+/**
+ * Gets a random element from the given array
+ * @param  {*[]} a Any array
+ * @param  {number} s Any array
+ * @return {*[][]} Random element from input array
+ */
+function chunk (a, s) {
+	return a.map((e, i) => i % s ? null : a.slice(i, i + s)).filter(e => e);
+}
+
+module.exports = { rInAr, shuffle, chunk };
